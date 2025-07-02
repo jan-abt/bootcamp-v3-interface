e3c5235f248d8d40e5783c0aef3ba6c6610ac205
jan-abt <jan.abt@gmail.com>
2025-07-02 16:55:45 +0700

    21. Side Navigation

--- app/layout.js
+++ app/layout.js
@@ -9,6 +9,7 @@ const lexend = Lexend({
 import MetaMaskProvider from "./components/providers/MetaMaskProvider";
 import StoreProvider from "./components/providers/StoreProvider";
 import TopNav from "./components/TopNav";
+import SideNav from "./components/SideNav";
 
 export const metadata = {
   title: "DAPP Exchange",
@@ -21,6 +22,7 @@ export default function RootLayout({ children }) {
       <MetaMaskProvider>
         <html lang="en">
           <body className={`${lexend.className}`}>
+            <SideNav/>
             <main className="content">
               <TopNav />
               {children}
