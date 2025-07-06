10e594403708089493e0619f47f30c0c4d147128
jan-abt <jan.abt@gmail.com>
2025-07-06 20:26:35 +0700

    25. Order Book

--- app/components/Book.jsx
+++ app/components/Book.jsx
@@ -1,7 +1,49 @@
-function Book(){
+"use client"
+
+import { useState } from "react"
+import { ethers, formatUnits } from "ethers"
+
+// Redux
+import { useAppDispatch, useAppSelector } from "@/lib/hooks"
+import { setBalance } from "@/lib/features/tokens/tokens"
+import { selectTokenAndBalances } from "@/lib/selectors"
+
+// Cusomthooks
+import { useProvider } from "@/app/hooks/useProvider"
+import { useTokens } from "@/app/hooks/useTokens"
+import { useExchange } from "@/app/hooks/useExchange"
+import { add } from "lodash"
+
+
+function Book({ caption, market, orders }) {
+
     return (
         <div className="table-wrapper">
-
+            <table>
+                <caption>{caption}</caption>
+                <thead>
+                    <tr>
+                        <th>{caption === "Selling" ? market[1].symbol :  market[0].symbol}</th>
+                        <th>{market[0].symbol}/{market[1].symbol}</th>
+                         <th>{caption === "Selling" ? market[0].symbol :  market[1].symbol}</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {orders.map((order, index) => (
+                        <tr key={index} role="link" tabIndex={0} aria-label="Fill Order">
+                            <td className={caption === "Selling" ? "gray" : undefined}>
+                                {ethers.formatUnits(order.amountGet, 18)}
+                            </td>
+                            <td className={order.type === "buy" ? "green" : "red"}>
+                                {order.price}
+                            </td>
+                            <td className={caption === "Buying" ? "gray" : undefined}>
+                                {ethers.formatUnits(order.amountGive, 18)}
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
     );
 }
--- app/page.js
+++ app/page.js
@@ -1,22 +1,111 @@
 "use client"
 
-
 import { useEffect, useRef, useState } from "react"
 import { ethers } from "ethers"
 
 // Import components
-import Chart from "./components/Chart"
-import Market from "./components/Market"
-import Tabs from "./components/Tabs"
-import Book from "./components/Book"
-import Orders from "./components/Orders"
+import Chart from "@/app/components/Chart"
+import Market from "@/app/components/Market"
+import Tabs from "@/app/components/Tabs"
+import Book from "@/app/components/Book"
+import Orders from "@/app/components/Orders"
+
+// Mock data
+import { myOpenOrders, filledOrders, myFilledOrders } from "@/app/data/orders"
+
+// Redux
+import { useAppDispatch, useAppSelector } from "@/lib/hooks"
+import {
+  setAllOrders,
+  setCancelledOrders,
+  setFilledOrders
+} from "@/lib/features/exchange/exchange"
+
+// Custom hooks
+import { useProvider } from "@/app/hooks/useProvider"
+import { useExchange } from "@/app/hooks/useExchange"
+
+// Config
+import { config } from "@/app/config.json"
+
+// 
+import {
+  selectMarket,
+  selectOpenOrders
+} from "@/lib/selectors"
 
 export default function Home() {
+
+  // Redux
+  const dispatch = useAppDispatch()
+  const market = useAppSelector(selectMarket)
+  const openOrders = useAppSelector(selectOpenOrders)
+
+  // Hooks
+  const { provider, chainId } = useProvider()
+  const { exchange } = useExchange()
+
+
+  async function getAllOrders() {
+    const block = await provider.getBlockNumber() // most recent block
+
+    // Fetch orders ever created
+    const createdOrdersStream = await exchange.queryFilter("OrderCreated", 0, block)
+    const createdOrders = createdOrdersStream.map(event => event.args)
+    // Set all orders in Redux
+    dispatch(setAllOrders(serializeOrders(createdOrders)))
+
+    // Fetch cancelled orders
+    const cancelledOrdersStream = await exchange.queryFilter("OrderCancelled", 0, block)
+    const cancelledOrders = cancelledOrdersStream.map(event => event.args)
+    
+    // Set cancelled orders in Redux
+    dispatch(setCancelledOrders(serializeOrders(cancelledOrders)))
+
+    // Fetch filled orders
+    const filledOrdersStream = await exchange.queryFilter("OrderFilled", 0, block)
+    const filledOrders = filledOrdersStream.map(event => event.args)
+    
+    // Set filled orders in Redux
+    dispatch(setFilledOrders(serializeOrders(filledOrders)))
+  }
+
+  /**
+   *  Redux can not naturally serialize BigInts(uint256s from Solidity).
+   *  Therefore, we serialize them to formatted strings  
+   */
+  function serializeOrders(orders) {
+    let serializedOrders = []
+
+    orders.forEach(o => {
+      
+      serializedOrders[Number(o.id) - 1] = {
+        id: o.id.toString(),
+        user: o.user,
+        tokenGet: o.tokenGet,
+        amountGet: o.amountGet.toString(),
+        tokenGive: o.tokenGive,
+        amountGive: o.amountGive.toString(),
+        timestamp: o.timestamp.toString()
+      }
+    })
+    return serializedOrders
+
+  }
+
+  useEffect(() => {
+    if (provider && exchange && market) {
+      getAllOrders()
+
+    }
+
+  }, [provider, exchange, market])
+
   return (
     <div className="page trading">
       <h1 className="title">Trading</h1>
       <section className="insights">
-        <Chart/>
+        <Chart />
       </section>
 
       <section className="market">
@@ -34,7 +123,17 @@ export default function Home() {
 
       <section className="orderbook">
         <h2>Order Book</h2>
-        <Book />
+        {/* {console.log(openOrders)} */}
+        {market ? (
+          <>
+            {/* SELLING */}
+            <Book caption={"Selling"} market={market} orders={openOrders.sellOrders} />
+            {/* BUYING */}
+            <Book caption={"Buying"} market={market} orders={openOrders.buyOrders} />
+          </>) : (
+          <p className="center">Please Select Market</p>
+        )}
+
       </section>
 
       <section className="orders">
